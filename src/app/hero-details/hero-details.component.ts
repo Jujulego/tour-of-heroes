cfd289a1a2eced2e7af7cb0e5d85e7c68a656224
Julien Capellari <julien.capellari@neoxia.com>
2020-03-02 17:15:17 +0100

    add: hero detail component

---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css']
+})
+export class HeroDetailsComponent {
+  // Inputs
+  @Input() hero: Hero;
+}
