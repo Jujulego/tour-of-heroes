2c6956d28e1da695bfcf22a2d0c429cddc7966a3
Julien Capellari <julien.capellari@neoxia.com>
2020-03-02 16:48:44 +0100

    add: hero data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Hero } from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
 })
 export class HeroesComponent {
   // Attributes
-  hero = 'Windstorm';
+  hero: Hero = { id: 1, name: 'Windstorm' };
 }
