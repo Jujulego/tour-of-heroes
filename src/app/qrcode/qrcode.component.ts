58408badfc9a7e7d4e28ad3885cdda1fc8082b44
Julien Capellari <julien.capellari@neoxia.com>
2020-03-12 17:53:12 +0100

    update: qrcode square design

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -5,6 +5,9 @@ import * as qrcode from 'qrcode';
 import { QRCodeErrorCorrectionLevel } from 'qrcode';
 import { Square } from './square';
 
+// Types
+export type SquareStyle = 'diamond' | 'dot' | 'circle' | 'square' | 'star';
+
 // Utils
 function inEyeFrame(x: number, y: number, size: number): boolean {
   return (x < 8 && y < 8) || (x < 8 && y >= size - 8) || (x >= size - 8 && y < 8);
@@ -21,6 +24,7 @@ export class QrcodeComponent implements OnInit {
   @Input() data: string;
   @Input() width: string;
   @Input() image: string;
+  @Input() squareStyle: SquareStyle;
 
   @Input() version: string;
   @Input() correction: QRCodeErrorCorrectionLevel;
@@ -51,6 +55,49 @@ export class QrcodeComponent implements OnInit {
   }
 
   // Methods
+  squarePath(square: Square): string {
+    const s = this.scale;
+    const x = square.x * this.scale;
+    const y = square.y * this.scale;
+
+    switch (this.squareStyle) {
+      case 'circle':
+        return `M ${x + s / 2} ${y} ` +
+          `a ${s / 2} ${s / 2} 0 1 0 0 ${s} ` +
+          `a ${s / 2} ${s / 2} 0 1 0 0 ${-s} ` +
+          `Z`;
+
+      case 'diamond':
+        return `M ${x + s / 2} ${y} ` +
+          `L ${x} ${y + s / 2} ` +
+          `L ${x + s / 2} ${y + s} ` +
+          `L ${x + s} ${y + s / 2} ` +
+          `Z`;
+
+      case 'dot':
+        return `M ${x + s / 2} ${y + s / 4} ` +
+          `a ${s / 4} ${s / 4} 0 1 0 0 ${s / 2} ` +
+          `a ${s / 4} ${s / 4} 0 1 0 0 ${-s / 2} ` +
+          `Z`;
+
+      case 'star':
+        return `M ${x + s / 2} ${y} ` +
+          `Q ${x + s * .4} ${y + s * .4} ${x} ${y + s / 2} ` +
+          `Q ${x + s * .4} ${y + s * .6} ${x + s / 2} ${y + s} ` +
+          `Q ${x + s * .6} ${y + s * .6} ${x + s} ${y + s / 2} ` +
+          `Q ${x + s * .6} ${y + s * .4} ${x + s / 2} ${y} ` +
+          `Z`;
+
+      case 'square':
+      default:
+        return `M ${x} ${y} ` +
+          `L ${x} ${y + s} ` +
+          `L ${x + s} ${y + s} ` +
+          `L ${x + s} ${y} ` +
+          `Z`;
+    }
+  }
+
   private generate() {
     const version = parseInt(this.version, 10) || 0;
 
