7f155ecb49e71d0228ba9f7bbecd860414b4dfc3
Julien Capellari <julien.capellari@neoxia.com>
2020-03-23 16:52:58 +0100

    add: appbar component

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppbarComponent } from './component/appbar/appbar.component';
+import { AppbarComponent } from './components/appbar/appbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { DrawerComponent } from './components/drawer/drawer.component';
 
 // Module
 @NgModule({
   declarations: [
-    AppbarComponent
+    AppbarComponent,
+    DrawerComponent
   ],
   exports: [
     AppbarComponent
