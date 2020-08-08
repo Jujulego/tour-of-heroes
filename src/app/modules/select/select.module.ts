44f9a05030f89e837083d9104a48aad692c862af
Julien Capellari <julien@capellari.net>
2020-08-08 11:53:09 +0200

    add: select module

---
+++ src/app/modules/select/select.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SelectComponent } from './components/select/select.component';
+import { OptionComponent } from './components/option/option.component';
+
+// Module
+@NgModule({
+  declarations: [
+    SelectComponent,
+    OptionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SelectModule { }
