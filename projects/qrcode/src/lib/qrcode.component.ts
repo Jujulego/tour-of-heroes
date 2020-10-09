ace0c1b24f52a73ed513b6e94df02fe9a16a3704
Julien Capellari <julien.capellari@neoxia.com>
2020-10-09 16:55:23 +0200

    add: generate library

---
+++ projects/qrcode/src/lib/qrcode.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-qrcode',
+  template: `
+    <p>
+      qrcode works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class QrcodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/qrcode/src/lib/qrcode.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { QrcodeComponent } from './qrcode.component';
+
+
+
+@NgModule({
+  declarations: [QrcodeComponent],
+  imports: [
+  ],
+  exports: [QrcodeComponent]
+})
+export class QrcodeModule { }
